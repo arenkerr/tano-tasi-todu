d16715f6028b3ef73cdd70e28cb662e186b5329d
Alana Kerr <ani.kerr@gmail.com>
2021-05-23 20:46:10 -0400

    update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,19 +1,19 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'mongoose',
+      connector: "mongoose",
       settings: {
-        host: env('DATABASE_HOST', 'cluster-hxxkmxhx.eybbx.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'tano'),
-        username: env('DATABASE_USERNAME', 'ani'),
-        password: env('DATABASE_PASSWORD', 'admin'),
+        host: env("DATABASE_HOST", "null"),
+        srv: env.bool("DATABASE_SRV", true),
+        port: env.int("DATABASE_PORT", 27017),
+        database: env("DATABASE_NAME", "null"),
+        username: env("DATABASE_USERNAME", "null"),
+        password: env("DATABASE_PASSWORD", "null"),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
+        authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+        ssl: env.bool("DATABASE_SSL", true),
       },
     },
   },
