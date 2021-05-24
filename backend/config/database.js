ef48a3e06644cfe8778f9871e1f73db7cace910b
Alana Kerr <ani.kerr@gmail.com>
2021-05-23 20:50:10 -0400

    fix quotes

--- backend/config/database.js
+++ backend/config/database.js
@@ -7,9 +7,9 @@ module.exports = ({ env }) => ({
         host: env("DATABASE_HOST", "null"),
         srv: env.bool("DATABASE_SRV", true),
         port: env.int("DATABASE_PORT", 27017),
-        database: env("DATABASE_NAME", "null"),
-        username: env("DATABASE_USERNAME", "null"),
-        password: env("DATABASE_PASSWORD", "null"),
+        database: env("DATABASE_NAME", null),
+        username: env("DATABASE_USERNAME", null),
+        password: env("DATABASE_PASSWORD", null),
       },
       options: {
         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
