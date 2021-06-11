892dffdfdc31ae741e30d3d78d0fe334a4775415
Alana Kerr <ani.kerr@gmail.com>
2021-06-11 16:48:42 -0400

    add contact page content

--- frontend/src/pages/{StrapiPage.slug}.tsx
+++ frontend/src/pages/{StrapiPage.slug}.tsx
@@ -10,10 +10,12 @@ import Description from '../components/description';
 import Footer from '../components/footer';
 import Spacer from '../components/spacer';
 import HorizontalList from '../components/horizontalList';
+// import Form from '../components/form';
 
 export const query = graphql`
   query PageQuery($slug: String!) {
     strapiPage(slug: { eq: $slug }) {
+      slug
       hero {
         title
         cover {
@@ -45,12 +47,15 @@ const Page = ({ data }) => {
   const page = data.strapiPage;
   const seo = data.strapiPage.seo;
   const classes = useStyles();
+  console.log(page.slug === 'contact');
   return (
     <Layout seo={seo}>
       <Hero hero={page.hero} />
       <Container>{page.content && <Description source={page.content.body} />}</Container>
       <Spacer />
       {page.horizontalList && <HorizontalList list={page.horizontalList.listItem} />}
+      <Spacer />
+      {/* {page.slug === 'contact' && <Form />} */}
       <Footer />
     </Layout>
   );
